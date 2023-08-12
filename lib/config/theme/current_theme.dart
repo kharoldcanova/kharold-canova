97785adb7b518c898dab946dc68b9cd56a791f27
kharoldcanova <kharoldsamir@gmail.com>
2023-08-12 11:28:14 -0500

    getRamdonColor

--- lib/config/theme/current_theme.dart
+++ lib/config/theme/current_theme.dart
@@ -1,13 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  List<Color> colors = [
+    Colors.deepOrange,
+    Colors.blueAccent[700]!,
+    Colors.greenAccent[700]!,
+    Colors.redAccent[400]!,
+  ];
+
+  Color getRandomColor() {
+    return colors[Random().nextInt(colors.length)];
+  }
+
   ThemeData getTheme() => ThemeData(
       colorScheme: ColorScheme(
         brightness: Brightness.light,
         primary: Colors.black,
         onPrimary: Colors.white,
-        secondary: Colors.blueAccent[700]!,
+        secondary: getRandomColor(),
         onSecondary: Colors.white,
         error: Colors.red,
         onError: Colors.white,
